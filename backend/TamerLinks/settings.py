20609ca8b4ded683442587013bd2c8b4ade41a0d
Alex-Ul <it.bumerang@gmail.com>
2019-06-02 20:14:04 +0300

    Remove admin page settings and enable api URLs

--- backend/TamerLinks/settings.py
+++ backend/TamerLinks/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['172.19.0.1', 'localhost', '127.0.0.1', 'api']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',

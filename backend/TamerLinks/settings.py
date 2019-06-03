14ab3dfd30b87e160cba9fafbee98757bfa53490
Alex-Ul <it.bumerang@gmail.com>
2019-06-03 06:42:09 +0300

    Rename environment variables for mysql instance

--- backend/TamerLinks/settings.py
+++ backend/TamerLinks/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'TamerLinks.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', ''),
-        'PORT': os.environ.get('DB_PORT', 3306)
+        'NAME': os.environ.get('MYSQL_DATABASE', ''),
+        'USER': os.environ.get('MYSQL_USER', ''),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', ''),
+        'HOST': os.environ.get('MYSQL_HOST', ''),
+        'PORT': os.environ.get('MYSQL_PORT', 3306)
     }
 }
 
